5029b7429ca65e0aa9f659b99b57f33c9e4da8c2
Maciej Samerdak <maciej.samerdak@codepoets.it>
2024-03-12 19:06:12 +0100

    Add SSL setup for HTTPS requests

--- web3_reverse_proxy/core/sockets/serversocket.py
+++ web3_reverse_proxy/core/sockets/serversocket.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 import socket
 import select
+import ssl
+import logging
 
-from web3_reverse_proxy.config.conf import LISTEN_BACKLOG_PARAM, PROXY_LISTEN_ADDRESS
+from web3_reverse_proxy.config.conf import LISTEN_BACKLOG_PARAM, PROXY_LISTEN_ADDRESS, SSL_ENABLED, \
+    SSL_CERT_FILE, SSL_KEY_FILE
 from web3_reverse_proxy.core.sockets.clientsocket import ClientSocket
 
 
@@ -20,8 +23,12 @@ class ServerSocket:
 
         s_read, _, _ = select.select([self.socket], [], [], timeout)
         if len(s_read) > 0:
-            s_src, _ = self.socket.accept()
-            res = ClientSocket.from_socket(s_src)
+            try:
+                s_src, _ = self.socket.accept()
+                res = ClientSocket.from_socket(s_src)
+            except(ssl.SSLError) as ssl_err:
+                logging.error(ssl_err)
+                res = None
 
         return res
 
@@ -35,6 +42,10 @@ class ServerSocket:
         s_srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s_srv.bind((PROXY_LISTEN_ADDRESS, listen_port))
         s_srv.listen(listen_backlog_param)
+        if SSL_ENABLED:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            context.load_cert_chain(SSL_CERT_FILE, SSL_KEY_FILE)
+            s_srv = context.wrap_socket(s_srv, server_side=True)
 
         if timeout:
             s_srv.settimeout(timeout)
