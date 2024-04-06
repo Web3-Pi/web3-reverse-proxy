73ed00775406ce07e64c8d5a0debcf89377f3c38
Maciej Samerdak <maciej.samerdak@codepoets.it>
2024-04-06 14:13:16 +0200

    Modify endpoint components to handle chunk stream

--- web3_reverse_proxy/core/rpc/node/rpcendpoint/connection/endpointconnection.py
+++ web3_reverse_proxy/core/rpc/node/rpcendpoint/connection/endpointconnection.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import ssl
 from dataclasses import dataclass
+from typing import Callable
 
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.connectiondescr import EndpointConnectionDescriptor
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.endpointconnectionstats import EndpointConnectionStats
@@ -11,6 +12,8 @@ from web3_reverse_proxy.core.sockets.basesocket import BaseSocket
 
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.receiver import ResponseReceiver, ResponseReceiverSSL, ResponseReceiverGeth
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.sender import RequestSender
+
+from web3_reverse_proxy.utils.logger import get_logger
 
 
 @dataclass
@@ -28,6 +31,19 @@ class EndpointConnection:
 
     stats: EndpointConnectionStats
 
+    _logger = get_logger("EndpointConnection")
+
+    def _get_receiver_response_handler(
+            self,
+            req: RPCRequest,
+            req_bytes: bytearray,
+            outer_handler: Callable
+        ) -> Callable:
+        def receiver_response_handler(res_bytes: bytearray):
+            self.stats.update(req_bytes, res_bytes)
+            return outer_handler(RPCResponse(res_bytes, req))
+        return receiver_response_handler
+
     def reconnect(self):
         self.socket.close()
 
@@ -42,17 +58,17 @@ class EndpointConnection:
         self.req_sender.update_socket(self.socket)
         self.res_receiver.update_socket(self.socket)
 
-    def send_recv_roundtrip(self, req: RPCRequest) -> RPCResponse:
+    def send_recv_roundtrip(self, req: RPCRequest, callback: Callable) -> None:
+        self._logger.debug(f"{self} is sending request {req}")
         req_bytes = self.req_sender.send_request(req)
-        res_bytes = self.res_receiver.recv_response()
-
-        self.stats.update(req_bytes, res_bytes)
-
-        return RPCResponse(res_bytes, req)
+        self._logger.debug(f"{self} is receiving response")
+        self.res_receiver.recv_response(self._get_receiver_response_handler(req, req_bytes, callback))
 
     @classmethod
     def create(cls, conn_descr: EndpointConnectionDescriptor) -> EndpointConnection:
+        cls._logger.debug(f"Creating socket based on description: {conn_descr}")
         sock = BaseSocket.create_socket(conn_descr.host, conn_descr.port, conn_descr.is_ssl)
+        cls._logger.debug(f"Socket created for description: {conn_descr}")
         ip = sock.get_peer_name()[0]
 
         req_sender = RequestSender(sock, conn_descr.host, conn_descr.auth_key)
--- web3_reverse_proxy/core/rpc/node/rpcendpoint/connection/receiver.py
+++ web3_reverse_proxy/core/rpc/node/rpcendpoint/connection/receiver.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import Callable, Tuple
 
 from web3_reverse_proxy.core.sockets.basesocket import BaseSocket
 from web3_reverse_proxy.core.rpc.response.rpcresponse import RPCResponse
+from web3_reverse_proxy.config.conf import DEFAULT_RECV_BUF_SIZE
+from web3_reverse_proxy.utils.logger import get_logger
 
 
 class ResponseReceiver(ABC):
@@ -16,37 +19,106 @@ class ResponseReceiver(ABC):
 
 
 class ResponseReceiverGeth(ResponseReceiver):
+    _logger = get_logger("ResponseReceiverGeth")
 
     def __init__(self, sock: BaseSocket) -> None:
         self.socket = sock
 
-    def recv_response(self) -> bytearray:
+    def recv_response(self, callback: Callable) -> None:
         response_received = False
         raw_response = bytearray()
         chunked = False
 
+        chunked = False
+        buf_size = DEFAULT_RECV_BUF_SIZE
+
+        self._logger.debug("Loop starting")
         while not response_received:
             assert self.socket.is_ready_read()
 
-            data = self.socket.recv()
+            self._logger.debug("Reading response from socket")
+            data = self.socket.recv(buf_size)
             raw_response += data
 
             if not raw_response:
                 raise IOError
+            self._logger.debug(f"Raw response -> {raw_response}")
+            # Chunked responses
+            if chunked or RPCResponse.is_chunked(raw_response):
+                raw_response_array = raw_response.split(RPCResponse.CRLF_SEPARATOR)
 
-            if b"Transfer-Encoding: chunked" in data:
-                chunked = True
+                if not chunked:
+                    content_sparator_index = raw_response_array.index(b'')
+                    headers = raw_response_array[:content_sparator_index]
+                    raw_response_array = raw_response_array[content_sparator_index + 1:]
+                    chunked = True
 
-            # FIXME: this part requires a reliable approach
-            response_received = RPCResponse.hack_is_complete_raw_response(raw_response, chunked)
+                    headers_data = b""
+                    for header in headers:
+                        headers_data += header + RPCResponse.CRLF_SEPARATOR
+                    headers_data += RPCResponse.CRLF_SEPARATOR
+                    callback(headers_data)
 
-        return raw_response
+                if raw_response.endswith(RPCResponse.CRLF_SEPARATOR):
+                    # Remove extra empty element after split
+                    raw_response_array.pop(-1)
+
+                chunks, raw_response, buf_size, response_received = self._process_chunked_data(raw_response, raw_response_array)
+                for chunk in chunks:
+                    callback(chunk)
+
+            # Non-chunked responses
+            else:
+                response_received = RPCResponse.is_complete_raw_response(raw_response)
+                if response_received:
+                    self._logger.debug("Response completed")
+                    callback(raw_response)
+
 
     def update_socket(self, sock: BaseSocket) -> None:
         self.socket = sock
 
+    @staticmethod
+    def _process_chunked_data(raw_response: bytearray, chunked_elements: list) -> Tuple[list, bytearray, int, bool]:
+        response_completed = RPCResponse.is_complete_raw_response(raw_response)
+        target_chunk_content_length = 10 ** 6  # arbitrarily large number to compare against buffer size
+        current_chunk = bytearray()
+        current_chunk_content_length = 0
+
+        if not response_completed:
+            # full length/content pairs
+            if len(chunked_elements) % 2 == 0:
+                target_chunk_content_length = int(chunked_elements[-2], 16)
+                # incomplete last chunk
+                if len(chunked_elements[-1]) != target_chunk_content_length or \
+                        not raw_response.endswith(RPCResponse.CRLF_SEPARATOR) or \
+                        target_chunk_content_length == 0 and \
+                        not response_completed:
+                    current_chunk = chunked_elements[-2] + RPCResponse.CRLF_SEPARATOR + chunked_elements[-1]
+                    current_chunk_content_length = len(chunked_elements[-1])
+                    chunked_elements = chunked_elements[:-2]
+                # complete chunk, further length unknown
+                else:
+                    target_chunk_content_length = 10 ** 6
+            # missing last chunk content
+            elif raw_response.endswith(RPCResponse.CRLF_SEPARATOR):
+                target_chunk_content_length = int(chunked_elements[-1], 16)
+                current_chunk = chunked_elements.pop(-1) + RPCResponse.CRLF_SEPARATOR
+            # incomplete chunk length
+            else:
+                current_chunk = chunked_elements.pop(-1)
+
+        complete_chunks = [
+            chunked_elements[i] + RPCResponse.CRLF_SEPARATOR + chunked_elements[i + 1] + RPCResponse.CRLF_SEPARATOR \
+                for i in range(len(chunked_elements) // 2)
+        ]
+        buf_size = min(DEFAULT_RECV_BUF_SIZE, target_chunk_content_length + len(RPCResponse.CRLF_SEPARATOR) - current_chunk_content_length)
+
+        return complete_chunks, current_chunk, buf_size, response_completed
+
 
 class ResponseReceiverSSL(ResponseReceiver):
+    _logger = get_logger("ResponseReceiverSSL")
 
     MAX_LINE_LEN = 65536
 
@@ -55,7 +127,7 @@ class ResponseReceiverSSL(ResponseReceiver):
 
     # FIXME: this implementation is based on an assumption that the endpoint is a valid web3 endpoint (i.e. replies with
     #  valid responses only)
-    def recv_response(self) -> bytearray:
+    def recv_response(self, callback: Callable) -> bytearray:
         fd = self.rfile
         response_data = bytearray()
 
@@ -64,9 +136,12 @@ class ResponseReceiverSSL(ResponseReceiver):
         content_len = -1
         chunked = False
 
+        self._logger.debug("Receiver loop starting")
         while True:
+            self._logger.debug("Receiver reading response from socket")
             data = fd.readline(self.MAX_LINE_LEN + 1)
             response_data += data
+            self._logger.debug(f"response data -> {response_data}")
 
             if data in (b'\r\n', b'\n', b''):
                 break
@@ -74,7 +149,7 @@ class ResponseReceiverSSL(ResponseReceiver):
             if data.startswith(b'Content-Length:'):
                 content_len = int(data[16:])
 
-            if data == b'Transfer-Encoding: chunked\r\n':
+            if RPCResponse.is_chunked(response_data):
                 chunked = True
 
         if response_data == b'':
@@ -83,8 +158,11 @@ class ResponseReceiverSSL(ResponseReceiver):
         if content_len > 0:
             data = fd.read(content_len)
             response_data += data
+            callback(response_data)
         else:
             assert chunked, response_data
+            callback(response_data)
+            response_data = bytearray()
 
             while True:
                 data = fd.readline(self.MAX_LINE_LEN + 1)
@@ -94,10 +172,12 @@ class ResponseReceiverSSL(ResponseReceiver):
                 data = fd.read(chunk_len + 2)  # +2 to read '/r/n'
                 response_data += data
 
+                callback(response_data)
+
                 if chunk_len == 0:
                     break
 
-        return response_data
+                response_data = bytearray()
 
     def update_socket(self, sock: BaseSocket) -> None:
         self.rfile = sock.socket.makefile('rb', -1)
