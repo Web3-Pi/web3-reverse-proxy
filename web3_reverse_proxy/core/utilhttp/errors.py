2bf727ce8dbd1f2888c5b3247cb06efa950856a6
lukasz-glen <>
2024-04-23 13:30:13 +0200

    feat: httptools parser

--- web3_reverse_proxy/core/utilhttp/errors.py
+++ web3_reverse_proxy/core/utilhttp/errors.py
@@ -5,15 +5,19 @@ class ErrorResponses:
 
     # FIXME: json responses should contain a valid id sent in the corresponding request, right now "null" id is returned
     #  by default
-    ERROR_CODES = {400: 'Bad Request',
-                   401: 'Unauthorized',
-                   403: 'Forbidden',
-                   410: 'Gone',
-                   413: 'Payload Too Large',
-                   429: 'Too Many Requests',
-                   500: 'Internal Server Error',
-                   503: 'Service Unavailable'
-                   }
+    
+    # TODO: Review and remove unused error codes for HTTP and JSON-RPC
+    ERROR_CODES = {
+        400: 'Bad Request',
+        401: 'Unauthorized',
+        403: 'Forbidden',
+        405: 'Method Not Allowed',
+        410: 'Gone',
+        413: 'Payload Too Large',
+        429: 'Too Many Requests',
+        500: 'Internal Server Error',
+        503: 'Service Unavailable'
+    }
 
     SERVER = "rpi_servix/0.0.1"
 
@@ -24,7 +28,7 @@ class ErrorResponses:
     PE_PAYMENT_REQUIRED = 3
     PE_PAYLOAD_TOO_LARGE = 4
 
-    SERVER_ERROR_TEMPLATE = 'HTTP/1.1 {} {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\n'
+    HTTP_ERROR_TEMPLATE = 'HTTP/1.1 {} {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\n'
 
     BASIC_RESPONSE_TEMPLATE = 'HTTP/1.1 {} {}\r\nContent-Type: application/json\r\nDate: {}\r\nContent-Length: {' \
                               '}\r\n\r\n{}\n'
@@ -35,6 +39,13 @@ class ErrorResponses:
                                'nosniff\r\n\r\n{}\n'
 
     WEB3_JSON_TEMPLATE = '{{"jsonrpc":"2.0","id":{},"error":{{"code":{},"message":"{}"}}}}'
+
+    @classmethod
+    def http_error(cls, code: int, message: str = "") -> bytes:
+        length = len(message) + 1
+        error = cls.ERROR_CODES[code]
+        err_msg = cls.HTTP_ERROR_TEMPLATE.format(code, error, length, message)
+        return cls.to_bytes(err_msg)
 
     @classmethod
     def to_bytes(cls, data: str) -> bytes:
@@ -60,52 +71,35 @@ class ErrorResponses:
         return cls.to_bytes(err_msg)
 
     @classmethod
-    def bad_request_headers(cls, _id: int | str = None) -> bytes:
-        web3_err = -(cls.PROXY_ERROR_BASE_CODE + cls.PE_HEADERS_OFFSET)
-        return cls.bad_request_web3(web3_err, "Too many headers", _id)
-
-    @classmethod
-    def bad_request_invalid_request_format(cls, _id: int | str = None):
-        web3_err = -(cls.PROXY_ERROR_BASE_CODE + cls.PE_INVALID_FORMAT_OFFSET)
-        return cls.bad_request_web3(web3_err, "Invalid request format", _id)
-
-    @classmethod
-    def unauthorized_invalid_API_key(cls):
-        _now = cls.current_datetime()
-        _data = 'invalid authentication id'
-        _len = len(_data) + 1  # +1 to include \n from the template
-        err_msg = cls.UNAUTH_RESPONSE_TEMPLATE.format(_now, _len, _data)
-
-        return cls.to_bytes(err_msg)
-
-    @classmethod
     def forbidden_payment_required(cls, _id: int | str = None):
         web3_err = -(cls.PROXY_ERROR_BASE_CODE + cls.PE_PAYMENT_REQUIRED)
         return cls.bad_request_web3(web3_err, "You exceeded the limit of calls. Check your plan", _id)
 
     @classmethod
-    def payload_too_large(cls, _id: int | str = None):
-        web3_err = -(cls.PROXY_ERROR_BASE_CODE + cls.PE_PAYLOAD_TOO_LARGE)
-        return cls.bad_request_web3(web3_err, "Request payload too large", _id)
-
-    @classmethod
-    def internal_server_error(cls):
-        _data = ""
-        _len = len(_data) + 1  # +1 to include \n from the template
-
-        err_msg = cls.SERVER_ERROR_TEMPLATE.format(500, "Internal Server Error", _len, _data)
-
-        return cls.to_bytes(err_msg)
-
-    @classmethod
-    def service_unavailable(cls):
-        _data = "System overload of sorts (proxy run out of resources???). Try again in a few minutes"
-        _len = len(_data) + 1  # +1 to include \n from the template
-
-        err_msg = cls.SERVER_ERROR_TEMPLATE.format(503, "Service Unavailable", _len, _data)
-
-        return cls.to_bytes(err_msg)
-
-    @classmethod
     def parse_error(cls, _id: int | str = None):
         return cls.bad_request_web3(-32700, "Invalid JSON format", _id)
+
+    @classmethod
+    def http_internal_server_error(cls) -> bytes:
+        return cls.http_error(500)
+
+    @classmethod
+    def http_service_unavailable(cls) -> bytes:
+        return cls.http_error(503, "System overload of sorts (proxy run out of resources???). Try again in a few minutes")
+
+    @classmethod
+    def http_bad_request(cls, message: str = "") -> bytes:
+         return cls.http_error(400, message)
+
+    @classmethod
+    def http_method_not_allowed(cls) -> bytes:
+         return cls.http_error(405, "Only POST requests are accepted")
+
+    @classmethod
+    def unauthorized_invalid_API_key(cls) -> bytes:
+        _now = cls.current_datetime()
+        _data = 'invalid authentication id'
+        _len = len(_data) + 1
+        err_msg = cls.UNAUTH_RESPONSE_TEMPLATE.format(_now, _len, _data)
+
+        return cls.to_bytes(err_msg)
