b54a14a3b83b54682f011486707ea408af252163
Maciej Samerdak <maciej.samerdak@codepoets.it>
2024-05-08 00:35:26 +0200

    Add locks to connection pool for thread-safety

--- web3_reverse_proxy/core/rpc/node/connection_pool.py
+++ web3_reverse_proxy/core/rpc/node/connection_pool.py
@@ -1,8 +1,10 @@
+from abc import ABC
+
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.connection_handler import ConnectionHandler
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.endpointconnection import EndpointConnection
 
 
-class ConnectionPool():
+class ConnectionPool(ABC):
     def get(self) -> ConnectionHandler:
         pass
 
--- web3_reverse_proxy/core/rpc/node/endpoint_connection_pool.py
+++ web3_reverse_proxy/core/rpc/node/endpoint_connection_pool.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import Iterable, List, Tuple
+from typing import List, Tuple
+from threading import Lock
 
 from web3_reverse_proxy.core.interfaces.rpcnode import LoadBalancer
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.connectiondescr import EndpointConnectionDescriptor
@@ -10,14 +11,15 @@ from web3_reverse_proxy.core.rpc.request.rpcrequest import RPCRequest
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.connection_handler import ConnectionHandler
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.endpoint_connection_handler import EndpointConnectionHandler
 from web3_reverse_proxy.core.rpc.node.rpcendpoint.connection.endpointconnection import EndpointConnection
+from web3_reverse_proxy.core.rpc.node.connection_pool import ConnectionPool
 
 from web3_reverse_proxy.interfaces.servicestate import StateUpdater
 from web3_reverse_proxy.utils.logger import get_logger
 
 
-class EndpointConnectionPool():
+class EndpointConnectionPool(ConnectionPool):
     _logger = get_logger("EndpointConnectionPool")
-    MAX_CONNECTIONS = 1
+    MAX_CONNECTIONS = 10
 
     def __init__(
             self,
@@ -28,6 +30,7 @@ class EndpointConnectionPool():
         self.endpoints = []
         self.load_balancer = load_balancer
         self.connections = []
+        self.lock = Lock()
 
         for index in range(len(descriptors)):
             name, conn_descr = descriptors[index]
@@ -35,26 +38,41 @@ class EndpointConnectionPool():
             self._logger.debug(f"Creating endpoint {name} with connection {conn_descr}")
             self.endpoints.append(RPCEndpoint.create(name, conn_descr, state_updater))
 
-    def _refresh(self) -> None:
+    def _cleanup(self) -> None:
         while len(self.connections) > self.MAX_CONNECTIONS:
             connection = self.connections.pop()
-            connection.close()
+            try:
+                connection.close()
+            except OSError as error:
+                self._logger.error(error)
 
     def get(self) -> ConnectionHandler:
         self._logger.debug("Selecting endpoint connection from pool")
         if len(self.connections) == 0:
+            self._logger.debug("No existing connections available, establishing new connection")
             # TODO: Reconsider load balancers after new architecture is settled
             endpoint_index = self.load_balancer.get_queue_for_request(self.endpoints, RPCRequest())
             connection = self.endpoints[endpoint_index].create_connection()
         else:
+            self.lock.acquire()
             connection = self.connections.pop()
+            self.lock.release()
+        self._logger.debug(f"Return connection {connection}")
         return EndpointConnectionHandler(connection, self)
 
     def put(self, connection: EndpointConnection) -> None:
+        self._logger.debug(f"Putting connection {connection} to pool")
+        self.lock.acquire()
         self.connections.append(connection)
-        self._refresh()
+        self._cleanup()
+        self.lock.release()
 
     # Intended for cleaning after shutdown
     def close(self) -> None:
+        self.lock.acquire()
         for connection in self.connections:
-            connection.close()
+            try:
+                connection.close()
+            except OSError as error:
+                self._logger.error(error)
+        self.lock.release()
