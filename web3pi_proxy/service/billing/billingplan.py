12c3995d07cb6f3b03019db4183c24eb8f58c028
shadeofblue <blue@golem.network>
2024-07-09 17:14:39 +0200

    billing plan wip

--- web3pi_proxy/service/billing/billingplan.py
+++ web3pi_proxy/service/billing/billingplan.py
@@ -23,22 +23,6 @@ class SimplestBillingPlan:
         self.glm_byte_price = glm_byte_price
         self.user_priority = user_priority
 
-    def num_free_calls(self):
-        return self.no_free_calls
-
-    def num_free_bytes(self):
-        return self.no_free_bytes
-
-    def call_price_glm(self, method: str) -> float:
-        assert method is not None
-        return self.glm_call_price
-
-    def byte_price_glm(self) -> float:
-        return self.glm_byte_price
-
-    def get_user_priority(self) -> int:
-        return self.user_priority
-
     def to_dict(self) -> dict:
         return {
             self.NUM_FREE_CALLS: self.no_free_calls,
@@ -58,9 +42,3 @@ class SimplestBillingPlan:
         return SimplestBillingPlan(
             d[cls.NUM_FREE_CALLS], d[cls.NUM_FREE_BYTES], 0.0, 0.0, d[cls.USER_PRIORITY]
         )
-
-    @classmethod
-    def create_plan(
-        cls, no_free_calls: int, no_free_bytes: int, priority: int
-    ) -> SimplestBillingPlan:
-        return SimplestBillingPlan(no_free_calls, no_free_bytes, 0.0, 0.0, priority)
