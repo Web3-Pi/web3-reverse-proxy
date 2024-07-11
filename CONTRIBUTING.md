0a485536cc51ca3feb63133b5d1290b0ad89011b
shadeofblue <blue@golem.network>
2024-07-11 16:31:58 +0200

    add a poe task to create the test accounts, without having to choose the `DEV` mode.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -10,3 +10,17 @@ poe create_migrations
 The resultant migration files are added to `web3pi_proxy/db/migrations` directory. Be sure to add them to the repository alongside the changes to the models' code.
 
 All the migrations are automatically applied when you start the web3pi-proxy.
+
+## Test users
+
+While developing, you may wish to create some test users with some arbitrary billing plans.
+
+Please run:
+
+```
+poe init_test_accounts
+```
+
+Restart your web3pi-proxy if you had it running.
+
+This should create users with api keys of: `aaa`, `bbb`, `ccc`.
---
+++ web3pi_proxy/utils/test_accounts.py
@@ -0,0 +1,36 @@
+from web3pi_proxy.db.models import User, BillingPlan
+
+TEST_ACCOUNT_DATA = {
+    "aaa": {
+        "num_free_calls": 100_000_000,
+        "num_free_bytes": 1500 * 1024 ** 3,
+        "user_priority": 0,
+    },
+    "bbb": {
+        "num_free_calls": 1_000_000,
+        "num_free_bytes": 2 * 1024 ** 3,
+        "user_priority": 1,
+    },
+    "ccc": {
+        "num_free_calls": 1_000_000,
+        "num_free_bytes": 1 * 1024 ** 3,
+        "user_priority": 2,
+    },
+}
+
+
+def init_test_accounts():
+    print("Adding test accounts.")
+    for api_key, plan_args in TEST_ACCOUNT_DATA.items():
+        user, created = User.get_or_create(api_key=api_key)
+        if created:
+            print(
+                f"   Adding user: {api_key}, "
+                f"free_calls {plan_args.get('num_free_calls')}, "
+                f"free_bytes: {plan_args.get('num_free_bytes')}, "
+                f"priority: {plan_args.get('user_priority')}"
+            )
+            bp = BillingPlan(user=user, **plan_args)
+            bp.save()
+        else:
+            print(f"   User: {api_key} already exists.")
